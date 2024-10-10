dfc448428405c658b0f50b78b942c8e7e39ac9c8
AmiraGamal1 <amiragamal098@gmail.com>
2024-10-10 16:00:54 +0300

    commit all

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return floor of the number
+    """
+    return int(n)
