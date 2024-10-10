265ff285c55dbbc0b4d3a86ae3cc464c627ef3b9
AmiraGamal1 <amiragamal098@gmail.com>
2024-10-10 15:51:11 +0300

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function to add two floats
+    """
+    return a + b
